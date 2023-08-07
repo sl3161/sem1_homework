8e06a76c9ad06b7187ad0d615efc3d27a8054e19
«Zotov <sl3161@yandex.ru>
2023-08-08 00:18:17 +0300

    задача 8

---
+++ Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = new Random ().Next (1, 30);
+int count = 1;
+while (count<number)
+{
+    if (count%2 == 0)
+    {
+        Console.WriteLine (count);
+    }
+    count = count +1 ;
+}
+
