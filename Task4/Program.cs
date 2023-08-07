7fd88813816d723e38f3d2a417458204596b7780
«Zotov <sl3161@yandex.ru>
2023-08-07 23:50:45 +0300

    задача 4

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿int num1 = new Random().Next (-20, 20);
+int num2 = new Random().Next (-20, 20);
+int num3 = new Random().Next (-20, 20);
+int max = 0;
+if (num1 > max)
+{
+    max=num1;
+}
+if (num2 > max)
+{
+    max=num2;
+}
+if (num3 > max)
+{
+    max=num3;
+}
+Console.Write ("Самое большое число = ");
+Console.WriteLine (max);
