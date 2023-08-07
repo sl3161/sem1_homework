909db33adb0c2c5052bf9d37000808906863207e
«Zotov <sl3161@yandex.ru>
2023-08-07 23:23:07 +0300

    ..

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1 = new Random().Next (-20, 20);
+int num2 = new Random().Next (-20, 20);
+if (num1 < num2)
+{
+    Console.Write ("Меньшее число = ");
+    Console.WriteLine (num1);
+    Console.Write ("Большее число =");
+    Console.WriteLine (num2);
+}
+else
+{
+    Console.Write ("Меньшее число = ");
+    Console.WriteLine (num2);
+    Console.Write ("Большее число =");
+    Console.WriteLine (num1);
+}
