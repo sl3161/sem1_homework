cf299ddb1602c255ac16363056324cf2b8f7d70a
«Zotov <sl3161@yandex.ru>
2023-08-08 23:08:29 +0300

    через switch case

---
+++ Task15/Program.cs
@@ -0,0 +1,29 @@
+﻿System.Console.WriteLine("Введите число :");
+int number = int.Parse (Console.ReadLine ());
+switch (number) 
+{
+    case 1:
+    System.Console.WriteLine("не является");
+    return;
+    case 2:
+    System.Console.WriteLine("не является"); 
+    return;   
+    case 3:
+    System.Console.WriteLine("не является");
+    return;
+    case 4:
+    System.Console.WriteLine("не является");
+    return;
+    case 5:
+    System.Console.WriteLine("не является");
+    return;
+    case 6:
+    System.Console.WriteLine("является");  
+    return; 
+    case 7:
+    System.Console.WriteLine("является"); 
+    return;
+    default:
+    System.Console.WriteLine("Введено неверное числоб,введите число от 1 до 7");
+    return;
+}
