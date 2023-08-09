02ada269998d3c4550d6a736b2dfa31affe71e9d
«Zotov <sl3161@yandex.ru>
2023-08-09 19:25:10 +0300

    .

---
+++ Task10/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Ведите 3-х значное число");
+int number = int.Parse (Console.ReadLine());
+System.Console.WriteLine($"Вторая цифра введенного числа = {((number-number%10)/10)%10}");
