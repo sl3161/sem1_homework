4340c82322a0561762908803ded828072a137376
«Zotov <sl3161@yandex.ru>
2023-08-09 18:10:55 +0300

    3-я цифра любого числа

---
+++ Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число :");
+int number = int.Parse (Console.ReadLine());
+int digit = 0;
+while (number>1000)
+{
+    number = number/10;
+    digit = (number%10);
+    System.Console.WriteLine(number);
+    System.Console.WriteLine(digit);
+}
+
+System.Console.WriteLine($"3-я цифравведенного числа = {digit}");
