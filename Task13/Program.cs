539d575108d6ea1c3d6d5551851d61bea9adb449
«Zotov <sl3161@yandex.ru>
2023-08-09 18:41:14 +0300

    Любая цифра любого числа

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,12 +1,26 @@
-﻿Console.WriteLine("Введите число :");
+﻿//Чуть усложнил - поиск любой цифры ( номер позиции искомой цифры вводится) в любом числе 
+Console.WriteLine("Введите число :");
 int number = int.Parse (Console.ReadLine());
+System.Console.WriteLine("Введите порядковый номер цифры, которую необходимо найти , в числе ");
+int digit_position = int.Parse (Console.ReadLine());
 int digit = 0;
-while (number>1000)
+
+if (number >= Math.Pow (10, (digit_position-1)))
 {
+    while (number > Math.Pow (10, digit_position))
+    {
     number = number/10;
     digit = (number%10);
-    System.Console.WriteLine(number);
-    System.Console.WriteLine(digit);
+    
+    }
+    
+    
+}
+else
+{
+        System.Console.WriteLine($"У введенного числа нет {digit_position} цифры");
+        
 }
 
-System.Console.WriteLine($"3-я цифравведенного числа = {digit}");
+
+System.Console.WriteLine($" {digit_position} цифра введенного числа = {digit}");
