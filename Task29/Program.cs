4b7cb524019f8e4bb1af53656ef38b550c3d8a7d
«Zotov <sl3161@yandex.ru>
2023-08-31 00:35:57 +0300

    Задача 29 массив

---
+++ Task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+PrintArray (8, 2, 15 );
+
+void PrintArray (int a, int min, int max)
+{
+    int [] resultarr = new int[a];
+
+    for (int i = 0; i < a; i++)
+    {
+        resultarr[i] = new Random().Next ( min, max);
+        System.Console.Write ($"{resultarr[i]}  " );
+    }
+    
+}
