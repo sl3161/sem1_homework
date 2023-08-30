f9bc96711b7f43a3cbf97bd23db6b657fdcfaa2e
«Zotov <sl3161@yandex.ru>
2023-08-31 00:02:32 +0300

    .

---
+++ Task27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine(GetSumDigitInNumber (65));
+
+int GetSumDigitInNumber (int num)
+{
+    int sum = 0;
+    while (num>0)
+    {
+        sum = sum + num%10;
+        num = num/10;
+        
+    }
+    return sum;
+}
