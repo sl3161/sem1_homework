7df88d76764e5b347db55d496348404a138d960b
«Zotov <sl3161@yandex.ru>
2023-08-30 23:45:07 +0300

    .

---
+++ Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int res = GetAPowB (3, 3);
+
+System.Console.WriteLine(res);
+int GetAPowB (int a, int b)
+{
+    int result = a;
+    for (int count = 1; count < b; count++)
+    {
+        result *= a;
+    }
+    return result;
+    
+}
+
