15ca5d3d1d3398f1c2032f08364ec4fce228ca09
«Zotov <sl3161@yandex.ru>
2023-09-17 20:37:39 +0300

    нахождение строки с наименьшей суммой

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("Ведите 3-х значное число");
 int number = int.Parse (Console.ReadLine());
 System.Console.WriteLine($"Вторая цифра введенного числа = {((number-number%10)/10)%10}");
+Ваш контактный телефон
